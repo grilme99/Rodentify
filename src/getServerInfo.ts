34f5c952a6ffd34d7289868605f4c376126bd161
recanman <abboudalomari6@gmail.com>
2022-10-09 15:26:14 -0700

    Update getServerInfo.ts

--- src/getServerInfo.ts
+++ src/getServerInfo.ts
@@ -1,9 +1,5 @@
 import phin from 'phin'
-import { IJoinTicket } from './types/JoinTicketTypes'
-
-interface IAssetGameResponse {
-	joinScriptUrl: string
-}
+import { IAssetGameResponse, IJoinScript } from './types/JoinTicketTypes'
 
 export async function _GetJoinTicket(placeId: number | string, jobId: string, cookie: string) {
 	if (!placeId) throw new Error('Expected a placeId')
@@ -30,6 +26,8 @@ export async function _GetJoinTicket(placeId: number | string, jobId: string, co
 
 	if (initialRequest.statusCode === 200 && initialRequest.body) {
 		const body = initialRequest.body
+		console.log(body)
+
 		if (!body.jobId || !body.joinScript) throw 'No joinScript or jobId'
 		
 		return body.joinScript
@@ -43,6 +41,6 @@ export async function _GetServerData(placeId: number | string, jobId: string, co
 	if (!jobId) throw new Error('Expected a jobId')
 	if (!cookie) throw new Error('Expected a cookie')
 
-	const gameData: IJoinTicket = await _GetJoinTicket(placeId, jobId, cookie)
+	const gameData: IJoinScript = await _GetJoinTicket(placeId, jobId, cookie)
 	return gameData
 }
